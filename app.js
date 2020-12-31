27cd46fa380951dad95cb5862bf23db4839a123f
gmzi <gastonmazieres@hotmail.com>
2020-12-31 00:35:47 -0500

    countdown added

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 // Nasa image of the day
 async function getNasaImg() {
   let key = apis.nasa;
-  console.log(key);
   const response = await axios.get(
     'https://api.nasa.gov/planetary/apod?' + key
   );
-  console.log(response);
   const nasaDiv = document.querySelector('#nasa');
   const videoSource = document.querySelector('#video');
   if (response.data.media_type === 'video') {
@@ -27,7 +25,7 @@ async function getNasaImg() {
 getNasaImg();
 
 // Next Xspace Launch:
-async function getDateAndTime() {
+async function getNextLaunch() {
   const response = await axios.get(
     'https://api.spacexdata.com/v4/launches/next'
   );
@@ -43,11 +41,106 @@ async function getDateAndTime() {
   launchTime.innerText = `UTC time: ${time}`;
   missionName.innerText = `Mission name: ${response.data.name}`;
 
-  countdown(response.data.date_unix);
+  const launchTitle = document.querySelector('#launch-title');
+
+  const countD = document.querySelector('#countdown');
+  countdown(response.data.date_unix, 'countdown');
 }
 
-function countdown(time) {
+// crewed missions:
+async function getCrewedMissions() {
+  const response = await axios.get(
+    'https://api.spacexdata.com/v4/launches/upcoming'
+  );
+  const crewedMissions = document.querySelector('#crewed-missions');
+  for (let i = 0; i < response.data.length; i++) {
+    if (response.data[i].name.includes('Crew')) {
+      const name = response.data[i].name;
+      const date = response.data[i].date_local;
+      const crewMembers = response.data[i].crew;
+      const countDown = 'TODO COUNTDOWN';
+
+      const missDiv = document.createElement('div');
+      missDiv.classList.add('card');
+      missDiv.setAttribute('style', 'width: 100%;');
+
+      const cardBody = document.createElement('div');
+      cardBody.classList.add('card-body');
+
+      const missionName = document.createElement('h5');
+      missionName.classList.add('card-title');
+      missionName.classList.add('text-center');
+      missionName.innerText = name;
+
+      const missionDate = document.createElement('p');
+      missionDate.classList.add('card-text');
+      missionDate.innerText = date;
+
+      const missionCountdown = document.createElement('a');
+      missionCountdown.setAttribute('id', 'crewed-countdown');
+      missionCountdown.classList.add('btn');
+      missionCountdown.classList.add('btn-dark');
+      missionCountdown.classList.add('btn-sm');
+      missionCountdown.classList.add('d-block');
+      countdown(response.data[i].date_unix, 'crewed-countdown');
+
+      const crewTitle = document.createElement('h4');
+      crewTitle.innerText = 'Crew memebers';
+
+      crewTitle.append(missionCountdown);
+      missionDate.append(missionCountdown);
+      missionName.append(missionDate);
+      cardBody.append(missionName);
+      missDiv.append(cardBody);
+      crewedMissions.append(missDiv);
+
+      // const cardGroup = document.createElement('div');
+      // cardGroup.classList.add('card-group');
+
+      crewMembers.forEach(async function (val) {
+        const response = await axios.get(
+          `https://api.spacexdata.com/v4/crew/${val}`
+        );
+
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('card');
+
+        const image = document.createElement('img');
+        image.classList.add('card-img-top');
+        image.setAttribute('src', response.data.image);
+
+        const groupCardBody = document.createElement('div');
+        groupCardBody.classList.add('card-body');
+
+        const groupCardName = document.createElement('h5');
+        groupCardName.classList.add('card-title');
+        groupCardName.innerText = response.data.name;
+
+        const groupCardAgency = document.createElement('h6');
+        groupCardAgency.classList.add('card-title');
+        groupCardAgency.innerText = `Agency: ${response.data.agency}`;
+
+        cardDiv.append(image);
+        groupCardBody.append(groupCardName);
+        groupCardBody.append(groupCardAgency);
+        cardDiv.append(groupCardBody);
+
+        cardGroup.append(cardDiv);
+      });
+      const cardGroup = document.createElement('div');
+      cardGroup.classList.add('card-group');
+      cardBody.append(cardGroup);
+    }
+  }
+}
+
+getCrewedMissions();
+
+const countdown = (time, id) => {
   const countDownDate = new Date(time);
+  const placeholder = document.createElement('h3');
+  placeholder.setAttribute('id', 'countdown');
+
   let x = setInterval(function () {
     let now = Math.round(new Date().getTime() / 1000);
     let distance = countDownDate - now;
@@ -65,20 +158,22 @@ function countdown(time) {
     let seconds = '0' + date.getSeconds();
 
     // Will display time in 10:30:23 format
-    const formattedTime = `${days} days, ${hours} hours, ${minutes.substr(
+    let formattedTime = `${days} D : ${hours} H : ${minutes.substr(
       -2
-    )} minutes and ${seconds.substr(-2)} seconds`;
-    const countD = document.querySelector('#countdown');
+    )} M : ${seconds.substr(-2)} SS`;
+
+    const countD = document.querySelector(`#${id}`);
     countD.innerText = formattedTime;
 
-    if (distance < 0) {
-      clearInterval(x);
-      countD.innerText = 'launched';
-    }
+    // appendTo.append(placeholder);
+    // if (distance < 0) {
+    //   clearInterval(x);
+    //   countD.innerText = 'launched';
+    // }
   }, 1000);
-}
+};
 
-getDateAndTime();
+getNextLaunch();
 
 // canvas
 const canvas = document.getElementById('canvas');
