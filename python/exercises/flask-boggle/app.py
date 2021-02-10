a158e10d6c2f07fe97a1ffc7f80b8cc8a79a0f38
gmzi <gastonmazieres@hotmail.com>
2021-02-10 12:57:51 -0500

    added tests

--- python/exercises/flask-boggle/app.py
+++ python/exercises/flask-boggle/app.py
@@ -1,17 +1,25 @@
 from flask import Flask, request, render_template, redirect, flash, jsonify
 from flask.globals import session
 from boggle import Boggle
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'longtime'
 
+debug = DebugToolbarExtension(app)
+# to stop debugger:
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
 boggle_game = Boggle()
+
 
 guesses = []
 
 
 @app.route('/')
-def show_board_show_form_set_session():
+def board_and_session():
+    """calls Boggle method to create board, pass board to template 
+    and sets board key and value in session"""
     board = boggle_game.make_board()
     session['board'] = board
     return render_template('base.html', board=board)
@@ -19,6 +27,9 @@ def show_board_show_form_set_session():
 
 @app.route('/server')
 def server_checks():
+    """ takes word from form, calls Boggle method to check if word is
+    valid and function to check is word has been already taken, returns
+    json response to front end"""
     word = request.args['word']
     if is_repeated(word):
         msg = 'word taken'
@@ -30,6 +41,10 @@ def server_checks():
 
 @app.route('/stats', methods=['POST'])
 def stats():
+    """ Creates session key for record score, receives the score of 
+    each play, if it's a record high, stores it in session, 
+    otherwise keeps existing score.
+    Creates and store the number of games played in current browser"""
     current_score = int(request.json['score'])
     if 'score' not in session:
         session['score'] = current_score
--- python/exercises/flask-boggle/static/word-check.js
+++ python/exercises/flask-boggle/static/word-check.js
@@ -1,69 +1,83 @@
-const timeNum = document.querySelector('#time-num');
-const scoreNum = document.querySelector('#score-num');
-form = document.querySelector('form');
-let input = document.querySelector('#word');
-let feedback = document.querySelector('.feedback');
-current_score = 0;
+function game() {
+  const timeNum = document.querySelector('#time-num');
+  const scoreNum = document.querySelector('#score-num');
+  form = document.querySelector('form');
+  let input = document.querySelector('#word');
+  let feedback = document.querySelector('.feedback');
+  const btnAgain = document.querySelector('.btn-again button');
+  current_score = 0;
 
-function countDown(number) {
-  let sustractor = setInterval(function () {
-    number--;
-    if (number > 0) {
-      timeNum.innerText = number;
-    } else {
-      clearInterval(sustractor);
-      gameDone();
+  function countDown(number) {
+    let sustractor = setInterval(function () {
+      number--;
+      if (number > 0) {
+        timeNum.innerText = number;
+      } else {
+        clearInterval(sustractor);
+        gameDone();
+      }
+    }, 1000);
+  }
+
+  function gameActive() {
+    btnAgain.classList.add('hide');
+    btnSubmit = document.querySelector('input[type="submit"]');
+    let scoreNum = document.querySelector('#score-num');
+
+    form.addEventListener('submit', function (e) {
+      e.preventDefault();
+      checkIfValid(input.value);
+      input.value = null;
+    });
+
+    async function checkIfValid(word) {
+      if (word === '') {
+        msg = 'not-a-word';
+      } else {
+        const response = await axios.get('http://127.0.0.1:5000/server', {
+          params: { word: word },
+        });
+        msg = response.data.result;
+      }
+      displayFeedback(msg);
+      if (msg === 'well done!!') {
+        updateScore(word);
+      }
     }
-  }, 1000);
-}
 
-function gameActive() {
-  btnSubmit = document.querySelector('input[type="submit"]');
-  let scoreNum = document.querySelector('#score-num');
-
-  form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    checkIfValid(input.value);
-    input.value = null;
-  });
-
-  async function checkIfValid(word) {
-    if (word === '') {
-      msg = 'not-a-word';
-    } else {
-      const response = await axios.get('http://127.0.0.1:5000/server', {
-        params: { word: word },
-      });
-      msg = response.data.result;
+    function displayFeedback(message) {
+      const display = document.querySelector('.feedback h3');
+      display.innerText = message;
     }
-    displayFeedback(msg);
-    if (msg === 'well done!!') {
-      updateScore(word);
+
+    function updateScore(word) {
+      current_score += word.length;
+      scoreNum.innerText = current_score;
     }
   }
 
-  function displayFeedback(message) {
-    const display = document.querySelector('.feedback h3');
-    display.innerText = message;
-  }
-
-  function updateScore(word) {
-    current_score += word.length;
+  function gameDone() {
+    timeNum.innerText = 'time out';
     scoreNum.innerText = current_score;
+    form.classList.add('hide');
+    feedback.classList.add('hide');
+    sendScore(current_score);
+    playAgain();
   }
+
+  async function sendScore(score) {
+    const res = await axios.post('http://127.0.0.1:5000/stats', { score });
+  }
+
+  function playAgain() {
+    btnAgain.classList.remove('hide');
+    btnAgain.addEventListener('click', function () {
+      location.reload();
+    });
+  }
+
+  gameActive();
+  countDown(60);
 }
 
-function gameDone() {
-  timeNum.innerText = 'time out';
-  scoreNum.innerText = current_score;
-  form.classList.add('hide');
-  feedback.classList.add('hide');
-  sendScore(current_score);
-}
-
-async function sendScore(score) {
-  const res = await axios.post('http://127.0.0.1:5000/stats', { score });
-}
-
-gameActive();
-countDown(60);
+game();
