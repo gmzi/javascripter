cd31108b6b7f0696feff7e232e270d2be0f89215
gmzi <gastonmazieres@hotmail.com>
2021-04-06 14:00:39 -0400

    four facts

--- 0-exercises/js-exercises/numberFacts/app.js
+++ 0-exercises/js-exercises/numberFacts/app.js
@@ -6,12 +6,30 @@ function numberFacts() {
     let facts = axios
       .get(`${url}/${rangeStart}..${rangeEnd}`, { json: 'json' })
       .then((data) => {
-        fillHtml(data.data);
+        fillHtml(data.data, 'num-list');
       });
   }
 
-  function fillHtml(object) {
-    const list = document.querySelector('#num-list');
+  function favNumFacts(num) {
+    let allFacts = [];
+    const list = document.getElementById('fav-num-list');
+    for (let i = 0; i < 4; i++) {
+      allFacts.push(axios.get(`${url}/${num}`));
+    }
+    Promise.all(allFacts)
+      .then((data) => {
+        for (fact of data) {
+          console.log(fact.data);
+          item = document.createElement('li');
+          item.innerText = fact.data;
+          list.append(item);
+        }
+      })
+      .catch((err) => console.log(err));
+  }
+
+  function fillHtml(object, listId) {
+    const list = document.querySelector(`#${listId}`);
     for (k in object) {
       item = document.createElement('li');
       item.innerText = object[`${k}`];
@@ -19,6 +37,7 @@ function numberFacts() {
     }
   }
   getFacts(10, 15);
+  favNumFacts(55);
 }
 
 numberFacts();
